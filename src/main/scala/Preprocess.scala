f63a779f0983a8ecdd13c6003215d131f199f1cd
bkennedy06 <bkennedyeble@gmail.com>
2024-12-01 17:26:08 -0800

    Genre binary column implementation

--- src/main/scala/Preprocess.scala
+++ src/main/scala/Preprocess.scala
@@ -1,26 +1,53 @@
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 
+
 object Preprocess {
 
   def preprocess(spark: SparkSession): DataFrame = {
     val titles = spark.read.option("sep", "\t").option("header", "true").csv("data/title.basics.tsv")
     val ratings = spark.read.option("sep", "\t").option("header", "true").csv("data/title.ratings.tsv")
+
+    // Join titles and ratings on tconst
     val joinedData = titles.join(ratings, Seq("tconst"))
 
-    joinedData
+    // Filter data for bad entries
+    val filteredData = joinedData
       .filter(col("titleType") === "movie")
       .filter(col("isAdult") === "0")
       .filter(
         col("runtimeMinutes").isNotNull &&
           col("genres").isNotNull &&
           col("averageRating").isNotNull &&
-          col("numVotes") > 50 // Arbitrary number to reduce less popular entries
+          col("numVotes") > 50
       )
       .withColumn("runtimeMinutes", col("runtimeMinutes").cast("double"))
+
+    // Split genres into an array
+    val splitGenresDF = filteredData.withColumn("genreArray", split(col("genres"), ","))
+
+    // Get unique ones
+    val uniqueGenres = splitGenresDF
+      .select(explode(col("genreArray")).alias("genre"))
+      .distinct()
+      .collect()
+      .map(_.getString(0))
+
+    // Create binary columns for em
+    val binaryGenreDF = uniqueGenres.foldLeft(splitGenresDF) { (df, genre) =>
+      df.withColumn(genre, array_contains(col("genreArray"), genre).cast("int"))
+    }
+
+    // Genre array not necessary
+    val finalDF = binaryGenreDF.drop("genreArray")
+
+    // Write the final DataFrame to the /data folder so we don't need to rerun
+    finalDF.write.mode("overwrite").parquet("data/processed_movies.parquet")
+
+    finalDF
   }
 
-// Just for induvidual testing. Remove for final product
+  // Just for individual testing. Remove for final product
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder
       .appName("PreprocessorApp")
@@ -28,6 +55,6 @@ object Preprocess {
       .getOrCreate()
 
     val cleanedData = preprocess(spark)
-    cleanedData.show()
+    cleanedData.show(20, truncate = false)
   }
 }
